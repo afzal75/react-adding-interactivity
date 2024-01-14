a3935c3c9761f8b8ef72ce3eb69b7b495feb52cb
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-14 22:31:57 +0600

    Transforming from an array

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -1,45 +1,43 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// let nextId = 0;
+let nextId = 0;
 
-// export default function Apps() {
-//   const [name, setName] = useState("");
-//   const [artists, setArtists] = useState([]);
+export function Lists() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
 
-//   return (
-//     <>
-//       <h1>Inspiring sculptors:</h1>
-//       <input value={name} onChange={(e) => setName(e.target.value)} />
-//       <button
-//         onClick={() => {
-//           setArtists(
-//             // Replace the state
-//             [
-//               // with a new array
-//               ...artists, // that contains all the old items
-//               { id: nextId++, name: name }, // and one new item at the end
-//             ]
-//           );
-//           //   artists.push({
-//           //     id: nextId++,
-//           //     name: name,
-//           //   });
-//         }}
-//       >
-//         Add
-//       </button>
-//       <ul>
-//         {artists.map((artist) => (
-//           <li key={artist.id}>{artist.name}</li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          setArtists(
+            // Replace the state
+            [
+              // with a new array
+              ...artists, // that contains all the old items
+              { id: nextId++, name: name }, // and one new item at the end
+            ]
+          );
+          //   artists.push({
+          //     id: nextId++,
+          //     name: name,
+          //   });
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 // ! ========================Removing from an array================
-
-import { useState } from "react";
 
 let initialArtists = [
   { id: 0, name: "Marta Colvin Andrade" },
@@ -47,7 +45,7 @@ let initialArtists = [
   { id: 2, name: "Louise Nevelson" },
 ];
 
-export default function Apps() {
+export function List() {
   const [artists, setArtists] = useState(initialArtists);
 
   return (
@@ -67,6 +65,55 @@ export default function Apps() {
           </li>
         ))}
       </ul>
+    </>
+  );
+}
+
+// ! ======================Transforming from an array================
+
+let initialShapes = [
+  { id: 0, type: "circle", x: 50, y: 100 },
+  { id: 1, type: "square", x: 150, y: 100 },
+  { id: 2, type: "circle", x: 250, y: 100 },
+];
+
+export default function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
+
+  function handleClick() {
+    const nextShapes = shapes.map((shape) => {
+      if (shape.type === "square") {
+        // No change
+        return shape;
+      } else {
+        // Return a new circle 50px below
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // Re-render with the new array
+    setShapes(nextShapes);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Move circles down!</button>
+      {shapes.map((shape) => (
+        <div
+          key={shape.id}
+          style={{
+            background: "purple",
+            position: "absolute",
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === "circle" ? "50%" : "",
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
     </>
   );
 }
