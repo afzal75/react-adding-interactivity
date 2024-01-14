bfdad5b30e7f1ffbcbe86795984440055ab17edb
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-14 22:40:34 +0600

    Replacing items in an array

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -77,7 +77,7 @@ let initialShapes = [
   { id: 2, type: "circle", x: 250, y: 100 },
 ];
 
-export default function ShapeEditor() {
+export function ShapeEditor() {
   const [shapes, setShapes] = useState(initialShapes);
 
   function handleClick() {
@@ -115,5 +115,43 @@ export default function ShapeEditor() {
         />
       ))}
     </>
+  );
+}
+
+// ! ========================Replacing items in an array================
+
+let initialCounters = [0, 0, 0];
+
+export function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
+
+  return (
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button
+            onClick={() => {
+              handleIncrementClick(i);
+            }}
+          >
+            +1
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
