33a8f6f0b3a56957aa7c828c407aceabf34782e7
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-14 13:45:32 +0600

    useState explained

--- src/App.jsx
+++ src/App.jsx
@@ -68,17 +68,86 @@
 //   );
 // }
 
-// ! preventDefault()
+// !preventDefault();
 
-export default function Signup() {
+// export default function Signup() {
+//   return (
+//     <form
+//       onSubmit={(e) => {
+//         e.preventDefault();
+//       }}
+//     >
+//       <input />
+//       <button>Send</button>
+//     </form>
+//   );
+// }
+
+// ! STATE
+
+// import { useState } from "react";
+// import { sculptureList } from "./data.js";
+
+// export default function Gallery() {
+//   const [index, setIndex] = useState(0);
+
+//   // let index = 0;
+
+//   function handleClick() {
+//     // index = index + 1;
+//     setIndex(index + 1);
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleClick}>Next</button>
+//       <h2>
+//         <i>{sculpture?.name} </i>
+//         by {sculpture?.artist}
+//       </h2>
+//       <h3>
+//         ({index + 1} of {sculptureList?.length})
+//       </h3>
+//       <img src={sculpture?.url} alt={sculpture.alt} />
+//       <p>{sculpture.description}</p>
+//     </>
+//   );
+// }
+
+// ! Multiple state using antoher example
+
+import { useState } from "react";
+import { sculptureList } from "./data.js";
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <input />
-      <button>Send</button>
-    </form>
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   );
 }
