82394d11cb449572b8430c656d8783e5316ba1c7
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-14 22:21:30 +0600

    remove from an array

---
+++ src/Apps.jsx
@@ -0,0 +1,72 @@
+// import { useState } from "react";
+
+// let nextId = 0;
+
+// export default function Apps() {
+//   const [name, setName] = useState("");
+//   const [artists, setArtists] = useState([]);
+
+//   return (
+//     <>
+//       <h1>Inspiring sculptors:</h1>
+//       <input value={name} onChange={(e) => setName(e.target.value)} />
+//       <button
+//         onClick={() => {
+//           setArtists(
+//             // Replace the state
+//             [
+//               // with a new array
+//               ...artists, // that contains all the old items
+//               { id: nextId++, name: name }, // and one new item at the end
+//             ]
+//           );
+//           //   artists.push({
+//           //     id: nextId++,
+//           //     name: name,
+//           //   });
+//         }}
+//       >
+//         Add
+//       </button>
+//       <ul>
+//         {artists.map((artist) => (
+//           <li key={artist.id}>{artist.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// ! ========================Removing from an array================
+
+import { useState } from "react";
+
+let initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
+
+export default function Apps() {
+  const [artists, setArtists] = useState(initialArtists);
+
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                setArtists(artists.filter((a) => a.id !== artist.id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
